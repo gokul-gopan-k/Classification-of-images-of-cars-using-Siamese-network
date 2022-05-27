0b33b9d7ada5b5a277e3b4dd3a072a1d3596bbce
gokul-gopan-k <gokul.gopan.k@gmail.com>
2022-05-27 19:28:29 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+class CONFIG:
+    data_path = "../input/algerian-used-cars/dataset/DATA"
+    BATCH_SIZE= 4
+    EPOCHS =50 
+    img_height= 224
+    img_width = 224
+    channel_width = 3
+    train_ratio = 0.75
+    validation_ratio = 0.15
+    test_ratio = 0.10
+    Embedding_dim =48
